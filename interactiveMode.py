0667afbd6a4bf31b63b9147160f898b8e5b24f55
AswinNasiketh <aswinvivekanandan@gmail.com>
2019-06-18 12:29:40 +0100

    made city lights dependent on power available in a cycle rather than battery level switched to using values file in place of constants made it harder to light all city blocks

--- interactiveMode.py
+++ interactiveMode.py
@@ -1,4 +1,5 @@
 import time
+import values
 
 class InteractiveMode():
 
@@ -10,27 +11,15 @@ class InteractiveMode():
         self.PV_THRESHOLD = currentSolarPanelVoltage + 0.01
         self.WINDMILL_THRESHOLD = 2.0
 
-        self.PV_GENERATION_UNIT = 5
+        self.PV_GENERATION_UNIT = 4
         self.WINDMILL_GENERATION_UNIT = 2
-        self.CONSUMPTION = 3
+
+        self.CITY_CONSUMPTION_1_BLOCK = 4
+        self.CITY_CONSUMPTION_ADDITIONAL_BLOCK = 1.5
 
         self.currentBatteryLevel = 0
         self.previousBatteryLevel = 0
-
-        self.LED_RED_DIM = (100, 0 , 0)
-        self.LED_YELLOW_MAX = (130, 130, 66)
-        self.LED_BLUE_MAX = (94, 193, 255)
-        self.LED_GREEN_BRIGHT = (97, 255, 94)
-        self.LED_WATER_BLUE = (94,155,255)
-        self.LED_CITY_LIGHTS_YELLOW = (163, 145, 44)
         
-        #city LED ranges
-        self.CITY_BLOCK_1_LOWER = 8
-        self.CITY_BLOCK_1_UPPER = 28
-        self.CITY_BLOCK_2_LOWER = 29
-        self.CITY_BLOCK_2_UPPER = 57
-        self.CITY_BLOCK_3_LOWER = 58
-        self.CITY_BLOCK_3_UPPER = 94
 
     def interactiveModeLoop(self):
         print("Starting interactive mode loop")
@@ -38,31 +27,33 @@ class InteractiveMode():
         self.currentBatteryLevel = 0
         self.previousBatteryLevel = 0
 
+        powerAvailableToConsume = 0
+        lightBlock1 = False
+        lightBlock2 = False
+        lightBlock3 = False
+
         while self.window.getTaskRunning():
             self.previousBatteryLevel = self.currentBatteryLevel
 
-            if self.currentBatteryLevel < 100:
+            if self.areSolarPanelsOn():
+                powerAvailableToConsume += self.PV_GENERATION_UNIT
 
-                if self.areSolarPanelsOn():
-                    self.currentBatteryLevel += self.PV_GENERATION_UNIT
+            powerAvailableToConsume += self.WINDMILL_GENERATION_UNIT * self.getWindmillsBlown()
 
-                self.currentBatteryLevel += self.WINDMILL_GENERATION_UNIT * self.getWindmillsBlown()
+            lightBlock1, powerAvailableToConsume = self.usePower(powerAvailableToConsume, self.CITY_CONSUMPTION_1_BLOCK)
+            lightBlock2, powerAvailableToConsume = self.usePower(powerAvailableToConsume, self.CITY_CONSUMPTION_ADDITIONAL_BLOCK)
+            lightBlock3, powerAvailableToConsume = self.usePower(powerAvailableToConsume, self.CITY_CONSUMPTION_ADDITIONAL_BLOCK)
 
-            if self.currentBatteryLevel > 100:
-                self.currentBatteryLevel = 100 #validation
 
-            if self.currentBatteryLevel > 0:
-                self.currentBatteryLevel -= self.CONSUMPTION
-
-            if self.currentBatteryLevel < 0:
-                self.currentBatteryLevel = 0 #validation
+            if powerAvailableToConsume > 0 :
+                self.storeEnergy(powerAvailableToConsume)
 
             self.animateBattery()
             self.animateReservoir()
-            self.animateCityLights()
+            self.animateCityLights(lightBlock1, lightBlock2, lightBlock3)
             print("Battery level: " + str(self.currentBatteryLevel))
             time.sleep(1)
-        
+
         self.board.resetBoard()
         
     
@@ -95,24 +86,24 @@ class InteractiveMode():
 
         #charging
         if batteryLevelChange > 0:
-            batteryLEDcolour = (int(self.LED_BLUE_MAX[0] * (self.currentBatteryLevel/100)),
-            int(self.LED_BLUE_MAX[1] * (self.currentBatteryLevel/100)),
-            int(self.LED_BLUE_MAX[2] * (self.currentBatteryLevel/100)))
+            batteryLEDcolour = (int(values.LED_BLUE_MAX[0] * (self.currentBatteryLevel/100)),
+            int(values.LED_BLUE_MAX[1] * (self.currentBatteryLevel/100)),
+            int(values.LED_BLUE_MAX[2] * (self.currentBatteryLevel/100)))
 
             self.board.setFuelCellLEDs(batteryLEDcolour)
             self.board.pulseFuelCell()
         elif batteryLevelChange < 0:
             #discharging
-            batteryLEDcolour = (int(self.LED_YELLOW_MAX[0] * (self.currentBatteryLevel/100)),
-            int((self.LED_YELLOW_MAX[1] * (self.currentBatteryLevel/100))),
-            int((self.LED_YELLOW_MAX[2] * (self.currentBatteryLevel/100))))
+            batteryLEDcolour = (int(values.LED_YELLOW_MAX[0] * (self.currentBatteryLevel/100)),
+            int((values.LED_YELLOW_MAX[1] * (self.currentBatteryLevel/100))),
+            int((values.LED_YELLOW_MAX[2] * (self.currentBatteryLevel/100))))
 
             self.board.setFuelCellLEDs(batteryLEDcolour)
         elif  batteryLevelChange == 0:
             if self.currentBatteryLevel == 0:
-                self.board.setFuelCellLEDs(self.LED_RED_DIM)
+                self.board.setFuelCellLEDs(values.LED_RED_DIM)
             elif self.currentBatteryLevel == 100:
-                self.board.setFuelCellLEDs(self.LED_GREEN_BRIGHT)
+                self.board.setFuelCellLEDs(values.LED_GREEN_BRIGHT)
         
     def animateReservoir(self):
         if self.currentBatteryLevel == 0:
@@ -121,31 +112,59 @@ class InteractiveMode():
             level2Colour = (0,0,0)
             level3Colour = (0,0,0)
         elif self.currentBatteryLevel > 0 and self.currentBatteryLevel < 25:
-            level0Colour = self.LED_WATER_BLUE
+            level0Colour = values.LED_WATER_BLUE
             level1Colour = (0,0,0)
             level2Colour = (0,0,0)
             level3Colour = (0,0,0)
         elif self.currentBatteryLevel >= 25 and self.currentBatteryLevel < 50:
-            level0Colour = self.LED_WATER_BLUE
-            level1Colour = self.LED_WATER_BLUE
+            level0Colour = values.LED_WATER_BLUE
+            level1Colour = values.LED_WATER_BLUE
             level2Colour = (0,0,0)
             level3Colour = (0,0,0)
         elif self.currentBatteryLevel >= 50 and self.currentBatteryLevel < 75:
-            level0Colour = self.LED_WATER_BLUE
-            level1Colour = self.LED_WATER_BLUE
-            level2Colour = self.LED_WATER_BLUE
+            level0Colour = values.LED_WATER_BLUE
+            level1Colour = values.LED_WATER_BLUE
+            level2Colour = values.LED_WATER_BLUE
             level3Colour = (0,0,0)
         elif self.currentBatteryLevel >= 75:
-            level0Colour = self.LED_WATER_BLUE
-            level1Colour = self.LED_WATER_BLUE
-            level2Colour = self.LED_WATER_BLUE
-            level3Colour = self.LED_WATER_BLUE
+            level0Colour = values.LED_WATER_BLUE
+            level1Colour = values.LED_WATER_BLUE
+            level2Colour = values.LED_WATER_BLUE
+            level3Colour = values.LED_WATER_BLUE
 
         self.board.setReservoirLEDs(level0Colour,level1Colour,level2Colour,level3Colour)
 
-    def animateCityLights(self):
-        cityLightsCoefficient = self.currentBatteryLevel/100.0
+
+    def animateCityLights(self, lightBlock1, lightBlock2, lightBlock3):
+        cityLightsCoefficient = 0.0
+
+        if lightBlock1:
+            cityLightsCoefficient += 0.33
+
+        if lightBlock2:
+            cityLightsCoefficient += 0.33
+
+        if lightBlock3:
+            cityLightsCoefficient += 0.33
+
         self.board.lightCityBlocks(cityLightsCoefficient)
 
 
-    
+    def usePower(self, powerAvailable, powerConsumed):
+        newPower = powerAvailable - powerConsumed
+        sufficientPower = True
+        if newPower < 0:
+            self.currentBatteryLevel  = self.currentBatteryLevel + newPower #newPower is negative, representing power deficiency 
+            if self.currentBatteryLevel < 0:
+                self.currentBatteryLevel = 0                
+                sufficientPower = False
+
+            newPower = 0
+
+        return sufficientPower, newPower
+
+    def storeEnergy(self, energyToStore):
+        self.currentBatteryLevel += energyToStore
+
+        if self.currentBatteryLevel > 100:
+            self.currentBatteryLevel = 100
