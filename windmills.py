10f6fb65dccace241c195ad839807c2c80f8a801
Aswin Vivekanandan <aswinvivekanandan@gmail.com>
2019-07-28 11:27:05 +0100

    created separate class for windmills

---
+++ windmills.py
@@ -0,0 +1,46 @@
+import time
+
+class Windmills():
+
+    DRIVE_FREQUENCY = 4 #Hz
+    DRIVE_POSITIVE_PIN = 1
+    DRIVE_NEGATIVE_PIN = 7
+
+    def __init__(self, pigpioHandle):
+        self.pi = pigpioHandle
+        self.driveWindmills = False
+        self.timeSinceLastUpdate = self.getTimeMilliseconds()
+        self.halfPeriod = 1000/DRIVE_FREQUENCY
+
+    def startWindmills(self):
+        self.driveWindmills = True
+        self.positivePinOn = False
+
+    def stopWindmills(self):
+        self.driveWindmills = False
+
+    def update(self):
+        timeElapsed = getTimeMilliseconds() - self.timeSinceLastUpdate
+        self.timeSinceLastUpdate = getTimeMilliseconds()
+
+        if self.driveWindmills:
+
+            if timeElapsed >= self.halfPeriod:
+                if self.positivePinOn:
+                    self.pi.write(DRIVE_POSITIVE_PIN, 0)
+                    self.pi.write(DRIVE_NEGATIVE_PIN, 1)
+                    self.positivePinOn = False
+                else:
+                    self.pi.write(DRIVE_POSITIVE_PIN, 1)
+                    self.pi.write(DRIVE_NEGATIVE_PIN, 0)
+                    self.positivePinOn = True
+
+        else:
+            self.pi.write(DRIVE_POSITIVE_PIN, 0)
+            self.pi.write(DRIVE_NEGATIVE_PIN, 0)                   
+
+    def getTimeMilliseconds(self):
+        return int(round(time.time() * 1000))
+
+
+    
