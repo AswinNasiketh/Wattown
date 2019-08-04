4b9b40d853f6f48109f4dd71c90202f1592e6db6
Aswin Vivekanandan <aswinvivekanandan@gmail.com>
2019-08-04 16:04:06 +0100

    added substation object to wattownboard class, added 'tripped' mode to city lightin

--- city.py
+++ city.py
@@ -12,26 +12,35 @@ class City():
         self.LEDHandle = LEDHandle
 
     #blocksToLight can take values from 0 - 3
-    def lightCityBlocks(self, blocksToLight):
+    def lightCityBlocks(self, blocksToLight, supplyTripped = False):
         if blocksToLight == 0:            
             for i in range(City.BLOCK_1_LOWER, City.BLOCK_3_UPPER + 1):
                 self.LEDHandle[i] = LED_RED_DIM
 
         elif blocksToLight == 1:
             for i in range(City.BLOCK_1_LOWER, City.BLOCK_1_UPPER + 1):
-                self.LEDHandle[i] = LED_CITY_LIGHTS_YELLOW
+                if supplyTripped and (i % 2):#if the supply is tripped, set all odd LEDs to red
+                    self.LEDHandle[i] = LED_RED_DIM
+                else:
+                    self.LEDHandle[i] = LED_CITY_LIGHTS_YELLOW
                 
             for i in range(City.BLOCK_2_LOWER, City.BLOCK_3_UPPER + 1):
                 self.LEDHandle[i] = (0,0,0)
         elif blocksToLight == 2:
             for i in range(City.BLOCK_1_LOWER, City.BLOCK_2_UPPER + 1):
-                self.LEDHandle[i] = LED_CITY_LIGHTS_YELLOW
+                if supplyTripped and (i % 2):
+                    self.LEDHandle[i] = LED_RED_DIM
+                else:
+                    self.LEDHandle[i] = LED_CITY_LIGHTS_YELLOW
                 
             for i in range(City.BLOCK_3_LOWER, City.BLOCK_3_UPPER + 1):
                 self.LEDHandle[i] = (0,0,0)
         elif blocksToLight == 3:
             for i in range(City.BLOCK_1_LOWER, City.BLOCK_3_UPPER + 1):
-                self.LEDHandle[i] = LED_CITY_LIGHTS_YELLOW
+                if supplyTripped and (i % 2):
+                    self.LEDHandle[i] = LED_RED_DIM
+                else:
+                    self.LEDHandle[i] = LED_CITY_LIGHTS_YELLOW
     
     #for more manual control
     def setCityLEDs(self, rangeLower, rangeUpper, colour):
